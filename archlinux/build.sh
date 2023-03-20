7f990fc817741d084bcb15c73b4410bb67306361
Hui-Hong You <hiroshi@ghostsinthelab.org>
2023-03-20 12:56:26 +0800

    Use Containerfile to build image directly without using host filesystem
    Signed-off-by: Hui-Hong You <hiroshi@ghostsinthelab.org>

--- archlinux/build.sh
+++ archlinux/build.sh
@@ -1,11 +1,24 @@
 #!/bin/env bash
 set -euo pipefail
 BASEDIR=$(dirname $0)
+TIMESTAMP=$(date +%Y%m%d%H%M)
 
 if [ ! -f ${BASEDIR}/archlinux-bootstrap-x86_64.tar.gz ]; then
   wget http://mirror.archlinux.tw/ArchLinux/iso/2023.03.01/archlinux-bootstrap-x86_64.tar.gz
 fi
 
-mkdir -p ${BASEDIR}/bootstrap_root
-sudo tar xvfz ${BASEDIR}/archlinux-bootstrap-x86_64.tar.gz --numeric-owner --strip-components=1 --directory ${BASEDIR}/bootstrap_root
-sudo tar cvf ${BASEDIR}/archlinux-baseimage.tar --directory ${BASEDIR}/bootstrap_root ${BASEDIR}
+if [ ! -f ${BASEDIR}/busybox ]; then
+  wget https://www.busybox.net/downloads/binaries/1.35.0-x86_64-linux-musl/busybox
+fi
+
+if [ -x "$(command -v podman)" ]; then
+  podman build --squash-all -f Containerfile -t archlinux:${TIMESTAMP}
+  podman image tag archlinux:${TIMESTAMP} archlinux:latest
+  exit 0
+fi
+
+if [ -x "$(command -v docker)" ]; then
+  docker build --squash -f Containerfile -t archlinux:${TIMESTAMP}
+  docker image tag archlinux:${TIMESTAMP} archlinux:latest
+  exit 0
+fi
--- archlinux/import-to-podman.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/env bash
-set -euo pipefail
-BASEDIR=$(dirname $0)
-
-cat archlinux-baseimage.tar | docker import - archlinux
