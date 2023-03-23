5bd4ee0fff589aa321993f4faa96648f37492c35
Hui-Hong You <hiroshi@ghostsinthelab.org>
2023-03-24 01:39:15 +0800

    I don't need busybox while building archlinux base image anymore
    Signed-off-by: Hui-Hong You <hiroshi@ghostsinthelab.org>

--- archlinux/build.sh
+++ archlinux/build.sh
@@ -7,26 +7,14 @@ if [ ! -f ${BASEDIR}/archlinux-bootstrap-x86_64.tar.gz ]; then
   wget http://mirror.archlinux.tw/ArchLinux/iso/2023.03.01/archlinux-bootstrap-x86_64.tar.gz
 fi
 
-if [ ! -f ${BASEDIR}/../common/busybox ]; then
-  echo "Need 'busybox' to continue the build process! Change directory to ../common/ and run 'make busybox' to download it!"
-  exit 1
-fi
-
-if [ ! -f ${BASEDIR}/busybox ]; then
-  cp ../common/busybox .
-  chmod u+x busybox
-fi
-
 if [ -x "$(command -v podman)" ]; then
   podman build --squash-all -f Containerfile -t archlinux:${TIMESTAMP}
   podman image tag archlinux:${TIMESTAMP} archlinux:latest
-  rm busybox
   exit 0
 fi
 
 if [ -x "$(command -v docker)" ]; then
   docker build --squash -f Containerfile -t archlinux:${TIMESTAMP}
   docker image tag archlinux:${TIMESTAMP} archlinux:latest
-  rm busybox
   exit 0
 fi
